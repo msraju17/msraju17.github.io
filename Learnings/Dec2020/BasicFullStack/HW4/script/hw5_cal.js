801c78ceaadfc821db651de8170d53a32327b35c
msraju17 <59378308+msraju17@users.noreply.github.com>
2020-12-01 22:48:23 -0500

    addition maths operations

--- Learnings/Dec2020/BasicFullStack/HW4/script/hw5_cal.js
+++ Learnings/Dec2020/BasicFullStack/HW4/script/hw5_cal.js
@@ -32,3 +32,54 @@ function printRem(){
     var second = parseInt(document.getElementById("svalue").value);
     alert("Addition of " + first + " and " + second + " is " + (first%second));
 }
+
+/*
+    Following function perform addition mathematicall operations
+*/
+//Check number is even
+function checkEven(){
+    var num = parseInt(document.getElementById("even").value);
+    if(num%2 === 0)
+    {
+        alert(num + " number is even");
+    }
+    else{
+        alert(num + " number is odd");
+    }
+    
+}
+
+//check number is  prime
+function checkPrime(){
+    var num = parseInt(document.getElementById("prime").value);
+    for(var i=2; i<Math.sqrt(num); i++)
+    {
+        if(num%i === 0)
+        {
+            alert(num + " is not a prime number. Because least number which divisible by " + i);
+            return;
+        }
+    }
+    alert(num + " is a prime number");
+}
+
+//check number is  palindrome
+function checkRev(){
+    var num = parseInt(document.getElementById("rev").value);
+    var temp = num;
+    var rev = 0;
+    while(num > 0)
+    {
+        var r = num%10;
+        rev = rev*10 + r;
+        num = Math.floor(num/10);
+    }
+    if(temp == rev)
+    {
+        alert(temp + " is a palindrome");
+    }
+    else{
+        alert(temp + " is not a palindrome because reverse of number is " + rev);
+    }
+    
+}
